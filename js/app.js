323e17e8fdaba93dbc8952b3769942f9a70f38b8
Leandro Simoneli <simonelileandro437@gmail.com>
2025-05-31 22:29:55 -0300

    Adicionando slides ao JS

--- js/app.js
+++ js/app.js
@@ -3,3 +3,16 @@ function toggleMenu() {
     nav.classList.toggle('show');
 
 }
+
+let slideindex = 0; 
+showSlides();
+function showSlides() {
+    const slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideindex++;
+    if (slideindex > slides.length) {slideindex = 1}    
+    slides[slideindex - 1].style.display = "block";  
+    setTimeout(showSlides, 2000);
+}
